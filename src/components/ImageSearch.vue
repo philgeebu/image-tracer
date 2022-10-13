bc6263e8e9b4ebb5f1047178b8be144d67f3459c
skrolz <>
2022-10-13 19:16:51 -0400

    search page in working state

--- src/components/ImageSearch.vue
+++ src/components/ImageSearch.vue
@@ -1,39 +1,16 @@
 <template>
     <div class="row justify-center">
-        <q-card class="q-my-md search-input-card">
+        <q-card class="q-my-md" style="width: 20rem">
             <q-card-section>
-                <q-form
-                    @submit="searchImageApi"
-                    @reset="onReset"
-                    class="q-gutter-md"
+                <q-input
+                    v-model="searchTerm"
+                    debounce="500"
+                    placeholder="Search for an image..."
                 >
-                    <q-input
-                        v-model="searchTerm"
-                        label="Search for an image..."
-                    />
-                    <div class="row justify-end">
-                        <q-btn
-                            glossy
-                            dense
-                            size="xl"
-                            class="handwritten-font q-px-lg"
-                            type="submit"
-                            color="green-9"
-                            text-color="grey-1"
-                            label="Submit"
-                        />
-                        <q-btn
-                            glossy
-                            dense
-                            size="xl"
-                            class="handwritten-font q-px-lg q-ml-sm"
-                            type="reset"
-                            color="grey-5"
-                            text-color="grey-14"
-                            label="Clear"
-                        />
-                    </div>
-                </q-form>
+                    <template v-slot:append>
+                        <q-icon name="search" />
+                    </template>
+                </q-input>
             </q-card-section>
         </q-card>
     </div>
@@ -52,34 +29,21 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 const searchTerm = ref('');
 const results = ref([]);
-const getImageUrl = (name) => {
+const getImageUrl = (name: string) => {
     return new URL(`${name}`, import.meta.url).href;
 };
-const searchImageApi = () => {
-    if (searchTerm.value) {
-        fetch(
-            `https://pixabay.com/api/?key=30198755-511fed12f4c341988f11b1a00&q=${encodeURIComponent(
-                searchTerm.value
-            )}&image_type=photo`
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                results.value = data.hits;
-            });
-    } else alert('Please enter a search term');
-};
-
-const onReset = () => {
-    searchTerm.value = '';
-    results.value = [];
-};
+watch(searchTerm, () => {
+    fetch(
+        `https://pixabay.com/api/?key=30198755-511fed12f4c341988f11b1a00&q=${encodeURIComponent(
+            searchTerm.value
+        )}&image_type=photo`
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            results.value = data.hits;
+        });
+});
 </script>
-
-<style scoped>
-.search-input-card {
-    width: 24em;
-}
-</style>
