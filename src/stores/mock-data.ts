5650b771da8722db1a9eb83ac399980e8737f79f
skrolz <>
2022-10-16 19:30:32 -0400

    moved mock data to own file

---
+++ src/stores/mock-data.ts
@@ -0,0 +1,19 @@
+import { Tracing } from '../models/main';
+
+export const TRACINGS: Tracing[] = [
+    {
+        imageID: 3010309,
+        previewURL:
+            'https://cdn.pixabay.com/photo/2017/12/10/17/00/robot-3010309_150.jpg',
+    },
+    {
+        imageID: 3609237,
+        previewURL:
+            'https://cdn.pixabay.com/photo/2018/08/15/22/24/bridge-3609237_150.jpg',
+    },
+    {
+        imageID: 413685,
+        previewURL:
+            'https://cdn.pixabay.com/photo/2014/08/08/20/51/hunger-413685_150.jpg',
+    },
+];
